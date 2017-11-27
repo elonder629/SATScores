74128f2e17ca6b87f1763dccffd33f07ab9bdfb4
Eric Londer <ericlonder730@gmail.com>
2017-11-26 23:21:27 -0500

    TAB IS THE BEST CHARACTER IN THE WORLD!!!!!!!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,38 +4,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static School[] schools = new School[1000];
+
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(new File("scores.txt"));
         String[] abc = new String[1000];
-
+        double overallAverage = 0;
         int maxIndx = -1;
-        School[] schools = new School[1000];
+        ArrayList<School> schools = new ArrayList<>();
         while (input.hasNext()) {
             maxIndx++;
             abc[maxIndx] = input.nextLine();
         }
 
-        for (int j = 1; j <= maxIndx; j++) {
-            schools[j] = new School(abc[j]);
+        for (int j = 0; j < maxIndx; j++) {
+            schools.add(j, new School(abc[j + 1]));
         }
-        for (int j = 1; j <= maxIndx; j++) {
-            int currentRank = 0;
-            for (int x = 1; x <= maxIndx; x++) {
-                if (schools[j].average <= schools[x].average && schools[j].Name.equals(schools[x].Name) == false) {
-                    currentRank++;
-                }
+
+        Collections.sort(schools, (s1, s2) ->
+                Integer.compare(s2.average, s1.average));
+        int max = 0;
+        for (School x : schools) {
+            if (x.average != 0){
+                max++;
+                overallAverage += x.average;
             }
-            schools[j].rank = currentRank;
-            System.out.println(currentRank);
-        }
-        School[] ranked = new School[1000];
-        for (int x = 1; x <= maxIndx;x++){
-            ranked[schools[x].rank] = schools[x];
+
 
         }
-        System.out.println(ranked[2].Name);
-
+        overallAverage = Math.round(overallAverage/max);
+        System.out.println("The overall average of NY is " + overallAverage);
+        System.out.println("The top three schools in NY are " + schools.get(0).Name + ", " + schools.get(1).Name + ", " + schools.get(2).Name);
     }
+
 
 }
--- src/com/company/School.java
+++ src/com/company/School.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.util.Scanner;
 
 /**
  * Created by el693 on 11/10/17.
@@ -9,9 +8,9 @@ public class School {
     public int SATMath;
     public int SATReading;
     public int SATWriting;
-    public double average;
+    public int average;
     public String Name = "";
-    public  int rank;
+
 
     public School(String scores) {
         String[] temp = scores.split("\t");
@@ -26,26 +25,22 @@ public class School {
             SATMath = 0;
             SATReading = 0;
             Name = temp[1];
-        }
-        else {
-            Scanner reading = new Scanner(temp[19]);
-            Scanner math = new Scanner(temp[18]);
-            Scanner writing = new Scanner(temp[20]);
-            SATReading = Integer.valueOf(temp[19]);
+        } else {
+            SATReading = Integer.parseInt(temp[19]);
             SATWriting = Integer.parseInt(temp[20]);
             SATMath = Integer.parseInt(temp[18]);
             Name = temp[1];
         }
         average = ((SATMath + SATReading + SATWriting));
-        System.out.println(SATReading);
-        System.out.println(SATWriting);
-        System.out.println(SATMath);
-        System.out.println(average);
-        System.out.println(Name);
+        System.out.println("\n" + Name + " \nTheir Average SAT Math Score is: " + SATMath + " \nTheir Average SAT Reading Score is: "
+                + SATReading + " \nTheir Average SAT Writing Score is: " + SATWriting + " \nTheir Average SAT Score is: " + average);
 
 
     }
 
 
 }
+
+
+
 
